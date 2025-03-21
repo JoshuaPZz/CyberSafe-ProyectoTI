9b894ebb1f24a774e0239ddc642c9668a8e70ef8
joshuaPZ <joshuaprieto8@gmail.com>
2025-03-21 10:10:54 -0500

    init

---
+++ demoCyberSafe/src/main/java/cyberSafe/demoCyberSafe/DemoCyberSafeApplication.java
@@ -0,0 +1,13 @@
+package cyberSafe.demoCyberSafe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoCyberSafeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoCyberSafeApplication.class, args);
+	}
+
+}
