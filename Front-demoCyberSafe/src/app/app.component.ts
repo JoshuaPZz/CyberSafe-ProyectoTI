e59ddde428f7cccbf8a04b0624a4ed32675de325
joshuaPZ <joshuaprieto8@gmail.com>
2025-04-01 23:20:31 -0500

    add: menu principal en el front de los mockups
    falta agregar imagenes

--- Front-demoCyberSafe/src/app/app.component.ts
+++ Front-demoCyberSafe/src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MenuInicioComponent } from './menu/menu-inicio/menu-inicio.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, MenuInicioComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'Front-demoCyberSafe';
---
+++ Front-demoCyberSafe/src/app/menu/menu-inicio/menu-inicio.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-menu-inicio',
+  imports: [],
+  templateUrl: './menu-inicio.component.html',
+  styleUrl: './menu-inicio.component.css'
+})
+export class MenuInicioComponent {
+
+}
