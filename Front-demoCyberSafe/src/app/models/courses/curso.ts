d359982771f4b6f22002e581af8a6781de674be7
amdres <ahenao.2005@gmail.com>
2025-05-31 13:13:51 -0500

    Push de carga de cursos home screen

---
+++ Front-demoCyberSafe/src/app/models/courses/curso.ts
@@ -0,0 +1,15 @@
+export interface Curso {
+    nombre: string;
+    descripcion: string;
+    duracion: string;
+    nombre_profesor: string;
+    numero_clases: number;
+    numero_quizes: number;
+    numero_de_estudiantes: number;
+    calificacion: number;
+    precio: number;
+    tipo_de_suscripcion: string;
+    url_foto_curso: string;
+    listado_lecciones: string[];
+    reviews: null;
+}
