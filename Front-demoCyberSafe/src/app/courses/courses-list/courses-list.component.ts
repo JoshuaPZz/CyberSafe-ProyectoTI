0b691d9961d8f486a3f70c8fb0867ba37053f103
David Rodriguez <rodriguezj.david@javeriana.edu.co>
2025-05-31 18:21:24 -0500

    Lista de cursos

--- Front-demoCyberSafe/src/app/courses/courses-list/courses-list.component.ts
+++ Front-demoCyberSafe/src/app/courses/courses-list/courses-list.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { Curso } from '../../models/courses/curso';
 import { Router } from '@angular/router';
 import { MenuHeaderComponent } from '../../menu/menu-header/menu-header.component';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 import { TopBarComponent } from "../../menu/top-bar/top-bar.component";
+import { CursosService } from '../../services/curso/cursos.service';
 
 @Component({
   selector: 'app-courses-list',
@@ -13,7 +15,14 @@ import { TopBarComponent } from "../../menu/top-bar/top-bar.component";
   styleUrl: './courses-list.component.css',
 })
 export class CoursesListComponent {
-  constructor(private router: Router) {}
+  private courses: Curso[] = []; 
+  constructor(private router: Router, private cursosService: CursosService) {}
+
+  ngOnInit(): void {
+      this.cursosService.getCursos().subscribe((data) => {
+        this.courses = data;
+      });
+    }
 
   navigateToView(event: Event) {
     event.preventDefault();
--- Front-demoCyberSafe/src/app/services/curso/cursos.service.ts
+++ Front-demoCyberSafe/src/app/services/curso/cursos.service.ts
@@ -3,7 +3,7 @@ import { AngularFireDatabase } from '@angular/fire/compat/database';
 import { from, map } from 'rxjs';
 
 import { Curso } from '../../models/courses/curso';
-import { Database, getDatabase, ref, get, child } from '@angular/fire/database';
+import { Database, getDatabase, ref, get, child, query, limitToFirst} from '@angular/fire/database';
 @Injectable({
   providedIn: 'root'
 })
@@ -13,10 +13,10 @@ export class CursosService {
 
   constructor() { }
 
-
     getCursos() {
     const cursosRef = ref(this.db, 'cursos');
-    return from(get(cursosRef)).pipe(
+    const limitedQuery = query(cursosRef, limitToFirst(6));
+    return from(get(limitedQuery)).pipe(
       map((snapshot) => {
         const data = snapshot.val();
         if (!data) return [];
