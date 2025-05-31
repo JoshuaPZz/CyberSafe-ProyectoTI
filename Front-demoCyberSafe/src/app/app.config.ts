f1f85ec65899f8a3a64fe5524c9def35a8e0786c
joshuaPZ <joshuaprieto8@gmail.com>
2025-05-31 12:46:13 -0500

    add: enviroment

--- Front-demoCyberSafe/src/app/app.config.ts
+++ Front-demoCyberSafe/src/app/app.config.ts
@@ -4,14 +4,26 @@ import {
   provideZoneChangeDetection,
 } from '@angular/core';
 import { provideRouter } from '@angular/router';
+import { provideHttpClient } from '@angular/common/http';
 
+import { provideFirebaseApp, initializeApp } from '@angular/fire/app';
+import { provideAuth, getAuth } from '@angular/fire/auth';
+import { provideFirestore, getFirestore } from '@angular/fire/firestore';
+import { provideDatabase, getDatabase } from '@angular/fire/database';
+import { provideStorage, getStorage } from '@angular/fire/storage';
+
+import { environment } from './enviroment';
 import { routes } from './app.routes';
-import { HttpClientModule } from '@angular/common/http';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
-    importProvidersFrom(HttpClientModule),
+    provideHttpClient(),
+    provideFirebaseApp(() => initializeApp(environment.firebase)),
+    provideAuth(() => getAuth()),
+    provideFirestore(() => getFirestore()),
+    provideDatabase(() => getDatabase()),
+    provideStorage(() => getStorage()),
   ],
 };
---
+++ Front-demoCyberSafe/src/app/enviroment.ts
@@ -0,0 +1,15 @@
+// environment.ts
+export const environment = {
+    production: false,
+    firebase: {
+      apiKey: "AIzaSyBeAKt7_oEvi8p4toyGxb4-i8FYsWu6_1k",
+      authDomain: "innovacion-ti-42951.firebaseapp.com",
+      databaseURL: "https://innovacion-ti-42951-default-rtdb.firebaseio.com",
+      projectId: "innovacion-ti-42951",
+      storageBucket: "innovacion-ti-42951.firebasestorage.app",
+      messagingSenderId: "300963441938",
+      appId: "1:300963441938:web:4be0106717c37d9f9f57cb",
+      measurementId: "G-2VC32PYYYK"
+    }
+  };
+  
